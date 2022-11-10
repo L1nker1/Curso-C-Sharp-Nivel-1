3ce6c66501c2295190a20efd1e3857a53bfb4b6b
linker <diegolucas142@gmail.com>
2022-11-10 16:21:01 -0300

    unidad 5

---
+++ Unidad 5/ejercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ejercicio2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, new_num = 0, mayor = 0;
+        int i = 1;
+
+        do{
+            Console.WriteLine("Ingrese un numero!");
+            num1 = int.Parse(Console.ReadLine());
+            if(mayor == 0) mayor = num1;  // tambien podria ser en un for cuando x valga 05
+            if(num1 > mayor) mayor = num1; 
+            i++;
+        }while(i<=10);
+        Console.WriteLine("el numero mayor es: "+ mayor);
+    }
+}
+
+//2. Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla.
+// Solo se debe emitir UN valor por pantalla.
---
+++ Unidad 5/ejercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ejercicio5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num, pares = 0, impares=0;
+        for(int i = 0; i< 5; i++ ){
+            Console.WriteLine("Ingrese un numero");
+            num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0){
+                if(pares==0) pares = num;
+                if(pares < num) pares = num;
+
+            }else if(num % 2 != 0){
+                if(impares == 0) impares = num;
+                if(impares > num) impares = num;
+            }
+        }
+        Console.WriteLine("El mayor de los pares es: "+pares);
+        Console.WriteLine("El menor de los impares es: "+impares);
+    }
+}
+
+//5. Hacer un programa que solicite 20 números y luego emitir por pantalla el máximo de los números pares y el mínimo de los números impares.
